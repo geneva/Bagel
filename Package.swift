0bfd9340a68af6716a8f26f5cad9fd93d79e3351
Michał Zaborowski <m1entus@gmail.com>
2022-12-01 17:54:47 +0100

    Update configuration

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
             name: "Bagel",
             dependencies: ["CocoaAsyncSocket"],
             path: "iOS/Source",
-            publicHeadersPath: ""
+            publicHeadersPath: "iOS/Source/Public"
         )
     ]
 )
